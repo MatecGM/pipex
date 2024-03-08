918cda8a2e670a7d31f27fee29971eed8425185d
Mateo Bico <mbico@3F4.42angouleme.fr>
2024-03-08 19:39:42 +0100

    file checker

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbico <mbico@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 08:49:11 by mbico             #+#    #+#             */
-/*   Updated: 2024/03/08 18:11:02 by mbico            ###   ########.fr       */
+/*   Updated: 2024/03/08 19:20:22 by mbico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,22 @@ void	ft_close(int error)
 	}
 }
 
-void	ft_file_checker(char *file)
+int	ft_file_checker(char *file)
 {
-	
+	if (access(file, F_OK) || access(file, X_OK))
+	{
+		perror(file);
+		return (1);
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv, char **env)
 {
-	t_data	*data;
+	//t_data	*data;
 
+	ft_file_checker(argv[1]);
+	(void) env;
 	if (argc != 4)
 		ft_close(1);
-	
 }
